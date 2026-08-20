5a441b677b5ada4dcfaf21d262e926ee344b6f8a
HUSKY <2316320995@qq.com>
2026-08-20 10:25:06 +0800

    Practice_14

---
+++ Practice_14/script.js
@@ -0,0 +1,19 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+        const increment = target / 200;
+
+        if (current < target) {
+            counter.innerText = `${Math.ceil(current + increment)}`;
+            setTimeout(updateCounter, 1);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCounter();   // ✅ 在循环内部启动更新
+});
