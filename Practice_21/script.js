6a16dcf34f65e8a613b587ac3458e1459467619d
HUSKY <2316320995@qq.com>
2026-08-23 15:09:36 +0800

    Practice_21

---
+++ Practice_21/script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('.card-header');
+const card_title = document.querySelector('.card-title');
+const card_description = document.querySelector('.card-description');
+const profile_img = document.querySelector('.profile-img');
+const author_strong = document.querySelector('.author-info strong');
+const author_small = document.querySelector('author-info small');
+
