cd9e7aba0e7deb4bb18e834f2d949538d8bcb17a
HUSKY <2316320995@qq.com>
2026-08-25 11:35:51 +0800

    Practice_24

---
+++ Practice_24/script.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector('#btn');
+const toasts_container = document.querySelector('#toasts');
+
+function createToast(message = null, type = null) {
+    const notif = document.createElement('div');
+    notif.classList.add('toast');
+    notif.classList.add(type ? type : getRandomType());
+    notif.innerText = message ? message : getRandomMessage();
+    toasts_container.appendChild(notif);
+    setTimeout(() => {
+        notif.remove()
+    }, 3000);
+}
+
+const messages = [
+    'Message One',
+    'Message Two',
+    'Message Three',
+    'Message Four',
+]
+
+const types = ['info', 'success', 'error']
+
+function getRandomMessage() {
+    return messages[(Math.floor(Math.random() * messages.length))];
+}
+
+function getRandomType()
+{
+    return types[(Math.floor(Math.random() * types.length))];
+}
+
+btn.addEventListener('click', () => {
+    createToast();
+})
