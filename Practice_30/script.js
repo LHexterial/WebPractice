4207163fdb1347eb2be653b34b1ee7033d1b0b6c
HUSKY <2316320995@qq.com>
2026-08-28 14:03:34 +0800

    Practice_30

---
+++ Practice_30/script.js
@@ -0,0 +1,45 @@
+const nums = document.querySelectorAll('.nums span');
+const counter = document.querySelector('.counter');
+const finalmsg = document.querySelector('.final');
+const replay = document.querySelector('#replay');
+
+function runAnimation()
+{
+    nums.forEach((num, idx) => {
+        const endIndex = nums.length - 1;
+        num.addEventListener('animationend', (e) => {
+            if (e.animationName === 'goIn' && idx !== endIndex)
+            {
+                num.classList.remove('in');
+                num.classList.add('out');
+            }
+            else if (e.animationName === 'goOut' && num.nextElementSibling)
+            {
+                num.nextElementSibling.classList.add('in');
+            }
+            else {
+            counter.classList.add('hide')
+            finalmsg.classList.add('show')
+      }
+        })
+    })
+
+}
+
+
+function resetDOM()
+{
+    counter.classList.remove('hide');
+    finalmsg.classList.remove('show');
+    nums.forEach((num) => {
+        num.classList.value = '';
+    })
+    nums[0].classList.add('in');
+}
+
+replay.addEventListener('click', () => {
+    resetDOM();
+    runAnimation();
+})
+
+runAnimation();
